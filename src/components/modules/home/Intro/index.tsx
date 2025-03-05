1363c4388524088f6a8b5732f184d93b3e9ce855
Rittika Dev <rittika.dev@gmail.com>
2025-03-05 12:33:30 +0600

    refactor: Updated login form, intro section, and added new assets

--- src/components/modules/home/Intro/index.tsx
+++ src/components/modules/home/Intro/index.tsx
@@ -15,87 +15,74 @@ const Intro = async () => {
 
 	return (
 		<SavorlyContainer className="my-20">
-			<section className="p-8 grid grid-cols-3 gap-8">
-				{/* Left Text Section */}
+			<section className="p-8 grid grid-cols-3 gap-10">
+				{/* LEFT TEXT SECTION */}
 				<div className="col-span-1 flex flex-col justify-center">
 					<h2 className="text-2xl tracking-wide font-bold bg-gradient-to-r from-rose-300 to-primary text-transparent bg-clip-text text-center md:text-left">
 						S A V O R L Y – Craft Your Crave
 					</h2>
 					<p className="mt-4 text-gray-600 text-sm leading-loose">
 						Savorly is a premium meal provider website that offers customers the
-						ability to browse, customize, and order meals tailored to their
+						ability to browse, customize, and order meals in accordance to their
 						preferences. Whether you’re looking for a quick lunch, a healthy
-						dinner, or a special dietary option, Savorly ensures a seamless and
-						personalized dining experience. The platform provides a diverse menu
-						with fresh, high-quality ingredients, allowing customers to build
-						their perfect meal with just a few clicks.
+						dinner, or a special dietary option, Savorly ensures a personalized
+						dining experience. The platform provides a diverse menu with fresh,
+						high-quality ingredients, allowing customers to build their perfect
+						meal with just a few clicks.
 					</p>
 				</div>
 
-				{/* Image Grid */}
+				{/* IMAGE GRID */}
 				<div className="col-span-2 grid grid-cols-3 gap-4">
-					{/* Left Images */}
+					{/* LEFT IMAGES */}
 					<div className="flex flex-col gap-4">
 						<Image
 							src={Slider8}
 							alt="Left Image 1"
-							width={300}
-							height={400}
+							width={250}
+							height={350}
 							className="rounded-xl object-cover w-full"
 						/>
 						<Image
 							src={Slider10}
 							alt="Left Image 2"
-							width={300}
-							height={400}
+							width={250}
+							height={350}
 							className="rounded-xl object-cover w-full"
 						/>
 					</div>
 
-					{/* Middle Image (Spans two rows) */}
+					{/* MIDDLE IMAGE (SPANS TWO ROWS) */}
 					<div className="row-span-2">
 						<Image
 							src={Slider7}
 							alt="Middle Image"
-							width={400}
-							height={780}
+							width={350}
+							height={730}
 							className="rounded-xl object-cover w-full "
 							style={{ height: "98%" }}
 						/>
 					</div>
 
-					{/* Right Images */}
+					{/* RIGHT IMAGES */}
 					<div className="flex flex-col gap-4">
 						<Image
 							src={Slider12}
 							alt="Right Image 1"
-							width={300}
-							height={400}
+							width={250}
+							height={350}
 							className="rounded-xl object-cover w-full"
 						/>
 						<Image
 							src={Slider11}
 							alt="Right Image 2"
-							width={300}
-							height={400}
+							width={250}
+							height={350}
 							className="rounded-xl object-cover w-full"
 						/>
 					</div>
 				</div>
 			</section>
-			{/* <div className="flex items-center justify-between">
-				<h2 className="text-3xl font-bold">Category</h2>
-				<Link href="/products">
-					<Button variant="outline" className="rounded-full">
-						View All
-					</Button>
-				</Link>
-			</div>
-			<div className="grid grid-cols-6 gap-6 mt-10">
-				{categories?.slice(0, 6).map((category: ICategory, idx: number) => (
-					<CategoryCard key={idx} category={category} />
-				))}
-			</div> */}
 		</SavorlyContainer>
 	);
 };
